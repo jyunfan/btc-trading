6b82863b586f4aabc9a5c7e4e1de204822ab01dc
Jyun-Fan Tsai <jftsai@appier.com>
2013-12-18 21:39:18 +0800

    Add virtual env

---
+++ src/arbitrage.py
@@ -0,0 +1,28 @@
+
+# Market order
+# { "exchange":"btc-e", "market":"LTC/BTC",
+#   "bid_price":"0.03", "bid_amount":"100", "ask_price":"0.035", "ask_amount":"70" }
+
+
+def get_market_orders(market):
+    pass
+
+def main():
+    # Loop
+
+    # Get market orders
+    market = 'LTC/BTC'
+    get_market_orders(market)
+
+    # Display current orders
+
+    # Select best pair of exchanges
+
+    # Execute
+
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
