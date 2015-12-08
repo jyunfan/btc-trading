8ff16261ecc9c23b51bb865740d5b3d9bc0ea03f
Jyun-Fan Tsai <jyunfan@gmail.com>
2015-12-08 21:27:28 +0800

    Add program parameters

--- src/rebalance.py
+++ src/rebalance.py
@@ -1,3 +1,5 @@
+#coding=utf-8
+
 """ Rebalance cash and BTC
 """
 
@@ -5,16 +7,23 @@ import argparse
 import time
 import logging
 from datetime import datetime
+import subprocess
+
 from Bitstamp import Bitstamp
 from Huobi import Huobi
 
 FORMAT = '%(asctime)-15s %(module)s %(message)s'
-#logging.basicConfig(level=logging.INFO, filename='rebalance.log', format=FORMAT)
-logging.basicConfig(level=logging.INFO, format=FORMAT)
+logging.basicConfig(level=logging.INFO, filename='rebalance.log', format=FORMAT)
+#logging.basicConfig(level=logging.INFO, format=FORMAT)
 
 
-def main(exchange_name, keyfile, fiat='usd', sleep_second=60):
-    min_trade_btc = 0.04
+def main(exchange_name, keyfile, min_trade_btc=0.01, min_price_change=1.01, fiat='usd', sleep_second=60):
+    min_trade_btc = 0.001
+    min_price_change = 1.001
+
+    # When account is balance, we create buy and sell limit order at expect price change
+    expect_price_change = 1.002
+
     coin = 'btc'
     amount_digits = 4
     price_digits = 2
@@ -28,9 +37,12 @@ def main(exchange_name, keyfile, fiat='usd', sleep_second=60):
         return
 
     while True:
-        logging.info("Start")
-        balance = exchange.balance()
+        logging.info("Loop")
 
+        logging.info("Cancel existing orders")
+        exchange.cancel_all_orders(coin)
+
+        balance = exchange.balance()
         fiat_amount = balance[fiat + '_balance']
         btc_amount = balance['btc_balance']
         logging.info('balance: FIAT: %.2f, BTC: %.2f' % (fiat_amount, btc_amount))
@@ -41,33 +53,51 @@ def main(exchange_name, keyfile, fiat='usd', sleep_second=60):
         logging.info('btc price: %.2f' % btc_price)
         btc_in_fiat = btc_amount * btc_price
 
-        if fiat_amount > btc_in_fiat * 1.1:
+        logging.info("FIAT/COIN=%.4f" % (fiat_amount / btc_in_fiat))
+        if fiat_amount > btc_in_fiat * min_price_change:
             btc_buy_amount = (fiat_amount - btc_in_fiat)/2.0/btc_price
             logging.info('Rebalance: buy BTC and sell FIAT.  Price = %.2f, amount = %.8f' % (btc_price, btc_buy_amount))
             if btc_buy_amount < min_trade_btc:
                 logging.info("Buy amount < min_trade_btc, skip")
             else:
-                open_orders = exchange.open_orders(coin)
-                logging.info("Open orders: %s" % str(open_orders))
-                logging.info("Cancel all orders")
-                exchange.cancel_all_orders(coin)
-                exchange.buy(round(btc_buy_amount, 8), round(btc_price, 2))
-        if fiat_amount < btc_in_fiat/1.1:
+                exchange.buy(round(btc_buy_amount, amount_digits), round(btc_price, price_digits))
+        elif fiat_amount < btc_in_fiat / min_price_change:
             btc_sell_amount = (btc_in_fiat - fiat_amount)/2.0/btc_price
             logging.info('Rebalance: sell BTC and buy FIAT.  Price = %.2f, amount = %.8f' % (btc_price, btc_sell_amount))
             if btc_sell_amount < min_trade_btc:
                 logging.info("Sell amount < min_trade_btc, skip")
             else:
-                open_orders = exchange.open_orders(coin)
-                logging.info("Open orders: %s" % str(open_orders))
-                logging.info("Cancel all orders")
-                exchange.cancel_all_orders(coin)
                 exchange.sell(round(btc_sell_amount, amount_digits), round(btc_price, price_digits))
+        else:
+            pass
+            # logging.info('Almost balance.  Put expected buy and sell limit order')
+            # # Expect price of coin fail
+            # expect_btc_price = btc_price / expect_price_change
+            # expect_btc_in_fiat = btc_in_fiat / expect_price_change
+            # btc_buy_amount = (fiat_amount - expect_btc_in_fiat)/2.0/expect_btc_price
+            # logging.info('Expected Rebalance: buy BTC and sell FIAT.  Price = %.2f, amount = %.8f' % (expect_btc_price, btc_buy_amount))
+            # if btc_buy_amount > min_trade_btc:
+            #     exchange.buy(round(btc_buy_amount, amount_digits), round(expect_btc_price, price_digits))
+            #
+            # # Expect price of coin raise
+            # expect_btc_price = btc_price * expect_price_change
+            # expect_btc_in_fiat = btc_in_fiat * expect_price_change
+            # btc_sell_amount = (expect_btc_in_fiat - fiat_amount)/2.0/expect_btc_price
+            # logging.info('Expected Rebalance: sell BTC and buy FIAT.  Price = %.2f, amount = %.8f' % (expect_btc_price, btc_sell_amount))
+            # if btc_sell_amount > min_trade_btc:
+            #     exchange.sell(round(btc_sell_amount, amount_digits), round(expect_btc_price, price_digits))
 
         time.sleep(sleep_second)
 
 
 if __name__ == '__main__':
-    #main("/home/jftsai/.btc-trading/key/bitstamp")
-    sleep = 20
-    main('huobi', "/home/jftsai/.btc-trading/key/huobi", 'cny', sleep)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-k', '--keyfile', required=True)
+    parser.add_argument('-f', '--fiat', required=True)
+    parser.add_argument('--exchange', required=True)
+    parser.add_argument('--min_trade_btc', required=True, type=float)
+    parser.add_argument('--min_price_change', required=True, type=float)
+    parser.add_argument('--sleep', required=True, type=int)
+    args = parser.parse_args()
+
+    main(args.exchange, args.keyfile, min_trade_btc=args.min_trade_btc, min_price_change=args.min_price_change, fiat=args.fiat, sleep_second=args.sleep)
