e485a12a4be0bea4799ef170de97917d1e59ab9d
Jyun-Fan Tsai <jyunfan@gmail.com>
2015-12-13 21:31:27 +0800

    Add Market class

--- src/cryptsy.py
+++ src/cryptsy.py
@@ -10,36 +10,36 @@ from market import Market
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger( __name__ )
 
-class MarketBtce(Market):
+class MarketCryptsy(Market):
     def __init__(self, market_id):
-        super(MarketBtce, self).__init__(market_id)
+        super(MarketCryptsy, self).__init__(market_id)
 
     def update_depth(self):
         if self.market_id == 'LTC/BTC':
-            market_str = 'ltc_btc'
-        elif self.market_id == 'BTC/USD':
-            market_str = 'btc_usd'
+            market_str = '3'
         else:
             raise Exception('Unknown market')
 
-        url = 'https://btc-e.com/api/2/%s/depth' % market_str
+        url = 'http://pubapi.cryptsy.com/api.php?method=singleorderdata&marketid=%s' % market_str
         self.depth = {}
         try:
-            r = requests.get(url)
-            self.depth = self.format_depth(r.json())
-        except ValueErorr as e:
+            r = requests.get(url).json()
+            depth = r['return'].values()[0]
+        except ValueError as e:
             logger.warn("Cannot get depth")
+        self.depth = self.format_depth(depth)
 
     def sort_and_format(self, l, reverse=False):
-        l.sort(key=lambda x: float(x[0]), reverse=reverse)
         r = []
         for i in l:
-            r.append({'price': float(i[0]), 'amount': float(i[1])})
+            r.append({'price': float(i['price']), 'amount': float(i['quantity'])})
+
+        r.sort(key=lambda x: x['price'], reverse=reverse)
         return r
 
     def format_depth(self, depth):
-        bids = self.sort_and_format(depth['bids'], True)
-        asks = self.sort_and_format(depth['asks'], False)
+        bids = self.sort_and_format(depth['buyorders'], True)
+        asks = self.sort_and_format(depth['sellorders'], False)
         return {'asks': asks, 'bids': bids}
 
 if __name__ == '__main__':
@@ -47,5 +47,5 @@ if __name__ == '__main__':
     parser.add_argument('-m', '--marketid', help='Market ID: e.g. LTC/BTC', default='LTC/BTC')
     args = parser.parse_args()
 
-    market = MarketBtce(args.marketid)
+    market = MarketCryptsy(args.marketid)
     print(market.get_ticker())
